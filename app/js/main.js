b572889fe1793c21704cc011992e49f6b5e19cf0
akriachko <artemkriachko@gmail.com>
2022-10-27 21:30:20 +0200

    added stycky header and and fixed the criteria

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 $(function () {
 
+	var mixer = mixitup('.popular__cards');
+
+	$(window).scroll(function () {
+		if ($(window).scrollTop() >= 75) {
+			$('.header').addClass('sticky')
+		} else {
+			$('.header').removeClass('sticky')
+		}
+	});
+
+	$(".menu").on("click", "a", function (e) {
+		e.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({ scrollTop: top }, 1500);
+	});
+
+
 });
